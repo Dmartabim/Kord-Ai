0898d978ca4e1a88505b5bd6d7f269cd3dad757f
Dmartabim <isaacileke@gmail.com>
2025-03-06 10:22:26 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,11 +1,11 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "1234564789", // your number with country code
-  OWNER_NAME: 'シ', // your owner name 
-  BOT_NAME: 'Kord AI', // your preferred bot name
-  SESSION_ID: '', // replace your_sessionid with yours and make sure its inside the 
-  INVINCIBLE_MODE: true, // toogle invincible mode 
+  OWNER_NUMBERS: "2348167544071", // your number with country code
+  OWNER_NAME: 'dmartabim', // your owner name 
+  BOT_NAME: 'Dmartabim', // your preferred bot name
+  SESSION_ID: 'kord_ai-7coCAgmtpHJT', // replace your_sessionid with yours and make sure its inside the 
+  INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: 'public',
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
